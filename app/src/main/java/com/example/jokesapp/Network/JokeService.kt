4f960318f9d824ad3362d89fdc93a04f4a028795
afgar88 <90805867+afgar88@users.noreply.github.com>
2022-03-26 18:24:34 -0400

    worked on Service

---
+++ app/src/main/java/com/example/jokesapp/Network/JokeService.kt
@@ -0,0 +1,23 @@
+package com.example.jokesapp.Network
+
+import com.example.jokesapp.model.Jokes
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface JokeService {
+
+    @GET(JOKES_PATH)
+    suspend fun getAllJokes(): Response<List<Jokes>>
+
+    @GET(RAMDOM_JOKES_PATH)
+    suspend fun getRandomJoke(): Response<Jokes>
+
+
+    companion object {
+        const val BASE_URL = "https://api.icndb.com/"
+        private const val JOKES_PATH = "jokes"
+
+        private const val RAMDOM_JOKES_PATH = "jokes/random"
+
+    }
+}
