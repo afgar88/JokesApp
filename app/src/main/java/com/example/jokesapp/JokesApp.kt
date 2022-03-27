611a793da0d775eb40c833c61d6b941ccdc20866
afgar88 <90805867+afgar88@users.noreply.github.com>
2022-03-27 18:20:35 -0400

    worked on DI

---
+++ app/src/main/java/com/example/jokesapp/JokesApp.kt
@@ -0,0 +1,17 @@
+package com.example.jokesapp
+
+import android.app.Application
+import com.example.jokesapp.di.applicationModule
+import com.example.jokesapp.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class JokesApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@JokesApp)
+            modules(listOf(networkModule, applicationModule))
+        }
+    }
+}
